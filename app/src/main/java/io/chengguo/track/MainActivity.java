cd27eaefd3c6e971543bee5835d425de79d6b2fe
FingerArt <george@chengguo.io>
2017-08-29 19:12:41 +0800

    Dev TrackView

--- app/src/main/java/io/chengguo/track/MainActivity.java
+++ app/src/main/java/io/chengguo/track/MainActivity.java
@@ -1,13 +1,63 @@
 package io.chengguo.track;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import io.chengguo.track.library.TrackView;
+
+public class MainActivity extends AppCompatActivity implements TrackView.SlideGraduationListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    protected TrackView track;
+    protected TextView time;
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        private int t;
+
+        @Override
+        public void handleMessage(Message msg) {
+            removeMessages(0);
+            if (t % 5 == 0) {
+                track.go(((int) (Math.random() * 100)));
+            } else {
+                track.go();
+            }
+            t++;
+            sendEmptyMessageDelayed(0, 10);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        track = (TrackView) findViewById(R.id.track);
+        time = (TextView) findViewById(R.id.time);
+        track.setOnSlideGraduationListener(this);
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public void onGraduationChanged(int currentGraduation) {//毫秒
+        Log.d(TAG, "onGraduationChanged() called with: currentGraduation = [" + currentGraduation + "]");
+        int min = currentGraduation / 6000;
+        int sec = (currentGraduation - min * 6000) / 100;
+        time.setText(String.format("%02d:%02d.%02d", min, sec, currentGraduation % 100));
+    }
+
+    public void dw(View view) {
+        handler.sendEmptyMessage(0);
+        track.lock(true);
+    }
+
+    public void td(View view) {
+        track.lock(false);
+        handler.removeMessages(0);
     }
 }
--- library/src/main/java/io/chengguo/track/library/TrackView.java
+++ library/src/main/java/io/chengguo/track/library/TrackView.java
@@ -1,4 +1,4 @@
-package io.chengguo.track;
+package io.chengguo.track.library;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -7,25 +7,32 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.annotation.AttrRes;
+import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author FingerArt http://fingerart.me
  * @date 2017年08月25日 10:10
  */
 public class TrackView extends FrameLayout {
-    protected static int offsetLeft;
-    protected static int offsetBigGraduation;
     private static final String TAG = TrackView.class.getSimpleName();
+    private static final boolean DEBUG = false;
+    private InnerScrollView innerScrollView;
+    private InnerTrackView track;
+    protected boolean lock;
 
     public TrackView(Context context) {
         this(context, null);
@@ -41,19 +48,51 @@ public class TrackView extends FrameLayout {
     }
 
     private void init(Context context, AttributeSet attrs, int defStyleAttr) {
-        InnerScrollView innerScrollView = new InnerScrollView(context);
-        CenterLineView centerLineView = new CenterLineView(context);
+        innerScrollView = new InnerScrollView(context);
         FrameLayout.LayoutParams params = generateDefaultLayoutParams();
-        params.width = 2;
+        params.width = 3;
         params.height = ViewGroup.LayoutParams.MATCH_PARENT;
         params.topMargin = 100;
         params.gravity = Gravity.CENTER_HORIZONTAL;
-        addView(innerScrollView, ViewGroup.LayoutParams.MATCH_PARENT, dipToPixels(context, 200));
-        addView(centerLineView, params);
+
+        addView(innerScrollView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        addView(new CenterLineView(context), params);
+
+        track = findViewById(innerScrollView, R.id.track_view);
+    }
+
+    public void go(int decibel) {
+        track.addDecibel(decibel);
+        go();
+    }
+
+    public void go() {
+        track.notifyDecibelChange();
+        post(new Runnable() {
+            @Override
+            public void run() {
+                innerScrollView.scrollToLast();
+            }
+        });
+    }
+
+    public void setOnSlideGraduationListener(SlideGraduationListener onSlideGraduationListener) {
+        innerScrollView.setOnSlideGraduationListener(onSlideGraduationListener);
+    }
+
+    public void lock(boolean lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return lock || super.onInterceptTouchEvent(ev);
     }
 
     public static class InnerScrollView extends HorizontalScrollView {
-        private static final String TAG = io.chengguo.track.TrackView.class.getSimpleName();
+        private static final String TAG = TrackView.class.getSimpleName();
+        InnerTrackView innerTrackView;
+        protected SlideGraduationListener graduationListener;
 
         public InnerScrollView(Context context) {
             this(context, null);
@@ -70,24 +109,41 @@ public class TrackView extends FrameLayout {
 
         private void initView(Context context, AttributeSet attrs, int defStyleAttr) {
             FrameLayout container = new FrameLayout(context);
-            InnerTrackView innerTrackView = new InnerTrackView(context);
+            innerTrackView = new InnerTrackView(context);
+            innerTrackView.setId(R.id.track_view);
             container.addView(innerTrackView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
             addView(container, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        }
+
+        public void setOnSlideGraduationListener(SlideGraduationListener listener) {
+            graduationListener = listener;
+        }
+
+        public void scrollToLast() {
+            scrollTo(innerTrackView.getMeasuredWidth(), 0);
         }
 
         @Override
         protected void onScrollChanged(int l, int t, int oldl, int oldt) {
             super.onScrollChanged(l, t, oldl, oldt);
-            Log.d(TAG, "onScrollChanged() called with: l = [" + l + "], t = [" + t + "], oldl = [" + oldl + "], oldt = [" + oldt + "]");
+            log(TAG, "onScrollChanged() called with: l = [" + l + "], t = [" + t + "], oldl = [" + oldl + "], oldt = [" + oldt + "]");
+            if (graduationListener != null) {
+                graduationListener.onGraduationChanged(l < 0 ? 0 : l);
+            }
         }
     }
 
-    private static class InnerTrackView extends View implements OnClickListener {
+    private static class InnerTrackView extends View {
 
         private static final String TAG = InnerTrackView.class.getSimpleName();
         private static final int UNIT = 50;
-        private int offsetTop = 100;
         private Paint mPaint;
+        private int offsetLeft;
+        private int offsetTop = 100;
+        private int offsetBigGraduation;
+        private int viewportW;
+        private int viewportH;
+        private ArrayList<Integer> decibels = new ArrayList<>();
 
         public InnerTrackView(@NonNull Context context) {
             this(context, null);
@@ -95,7 +151,6 @@ public class TrackView extends FrameLayout {
 
         public InnerTrackView(@NonNull Context context, @Nullable AttributeSet attrs) {
             this(context, attrs, 0);
-            setOnClickListener(this);
         }
 
         public InnerTrackView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
@@ -103,24 +158,40 @@ public class TrackView extends FrameLayout {
             mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         }
 
+        public void addDecibel(int decibel) {
+            decibels.add(decibel);
+        }
+
+        public void setDecibel(List<Integer> decibels) {
+            decibels.clear();
+            decibels.addAll(decibels);
+        }
+
+        public void clear() {
+            decibels.clear();
+        }
+
+        public void notifyDecibelChange() {
+            getLayoutParams().width = getMeasuredWidth() + 1;
+            requestLayout();
+        }
+
         @Override
         protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
             int sizeW = MeasureSpec.getSize(widthMeasureSpec);
             switch (MeasureSpec.getMode(widthMeasureSpec)) {
                 case MeasureSpec.EXACTLY:
-                    Log.d(TAG, "widthMeasureSpec: EXACTLY [" + sizeW + "]");
+                    log(TAG, "widthMeasureSpec: EXACTLY [" + sizeW + "]");
                     break;
                 case MeasureSpec.AT_MOST:
-                    Log.d(TAG, "widthMeasureSpec: AT_MOST [" + sizeW + "]");
+                    log(TAG, "widthMeasureSpec: AT_MOST [" + sizeW + "]");
                     break;
                 case MeasureSpec.UNSPECIFIED:
-                    Log.d(TAG, "widthMeasureSpec: UNSPECIFIED [" + sizeW + "]");
+                    log(TAG, "widthMeasureSpec: UNSPECIFIED [" + sizeW + "]");
                     break;
             }
 
             setMeasuredDimension(sizeW, MeasureSpec.getSize(heightMeasureSpec));
-//            viewport = ((View) getParent().getParent()).getMeasuredWidth();
-//            Log.d(TAG, "viewport: " + viewport);
         }
 
         /**
@@ -130,9 +201,10 @@ public class TrackView extends FrameLayout {
          */
         @Override
         protected void onDraw(Canvas canvas) {
-            Log.d(TAG, "onDraw() called with: canvas = [" + canvas + "]");
+            log(TAG, "onDraw() called with: canvas = [" + canvas + "]");
             drawBackground(canvas);
             drawGraduated(canvas);
+            drawTrack(canvas);
         }
 
         private void drawBackground(Canvas canvas) {
@@ -143,25 +215,27 @@ public class TrackView extends FrameLayout {
         private void drawGraduated(Canvas canvas) {
             int measuredWidth = getMeasuredWidth();
             if (offsetLeft == 0) {
+                viewportW = measuredWidth;
+                viewportH = getMeasuredHeight();
                 offsetLeft = (measuredWidth >> 1) % (UNIT << 2);//计算左边开始的大刻度偏移值
                 offsetBigGraduation = ((measuredWidth >> 1) - offsetLeft) / (UNIT << 2);
             }
             int count = (measuredWidth) / UNIT;//能显示的刻度总个数
-            Log.d(TAG, "count: " + count);
+            log(TAG, "count: " + count);
 
             int bigCount = ((count >> 2) + 1);
             float[] bigPoints = new float[bigCount << 2];
-            Log.d(TAG, "bigCount: " + bigCount);
+            log(TAG, "bigCount: " + bigCount);
 
             int smallCount = count - bigCount + 1;
-            Log.d(TAG, "smallCount: " + smallCount);
+            log(TAG, "smallCount: " + smallCount);
             float[] smallPoints = new float[smallCount << 2];
             for (int i = 0, x, y; i < count; i++) {
                 x = i * UNIT + offsetLeft;
                 if (i % 4 == 0) {
                     y = 40;
                     int bi = i >> 2;
-                    Log.d(TAG, i + " >> 2: " + bi);
+                    log(TAG, i + " >> 2: " + bi);
                     bigPoints[bi * 4] = x;
                     bigPoints[bi * 4 + 1] = offsetTop;
                     bigPoints[bi * 4 + 2] = x;
@@ -178,6 +252,8 @@ public class TrackView extends FrameLayout {
                     smallPoints[si * 4 + 3] = y + offsetTop;
                 }
             }
+
+            mPaint.setStrokeCap(Paint.Cap.ROUND);
             mPaint.setColor(Color.parseColor("#E4E4E4"));
             mPaint.setStyle(Paint.Style.FILL);
             canvas.drawLines(smallPoints, mPaint);//小刻度
@@ -189,24 +265,29 @@ public class TrackView extends FrameLayout {
         private void drawTime(Canvas canvas, int index, int x, int y) {
             mPaint.setTextSize(dipToPixels(getContext(), 12));
             mPaint.setStrokeWidth(3);
-            mPaint.setColor(Color.RED);
-            canvas.drawPoint(x, y, mPaint);
 
             String text = String.format("%02d:%02d", (index << 1) / 60, (index << 1) % 60);
             Rect rect = new Rect();
             mPaint.getTextBounds(text, 0, text.length(), rect);
-            Log.d(TAG, "rect: " + rect.toShortString());
+            log(TAG, "rect: " + rect.toShortString());
             mPaint.setColor(Color.parseColor("#808080"));
             int center = x - rect.centerX();
-            Log.d(TAG, "center: " + center);
+            log(TAG, "center: " + center);
             canvas.drawText(text, center, y, mPaint);
         }
 
-        @Override
-        public void onClick(View v) {
-            Log.d(TAG, "onClick() called with: v = [" + v + "]");
-            getLayoutParams().width = getMeasuredWidth() + 100;
-            requestLayout();
+        private void drawTrack(Canvas canvas) {
+            int zeroPoint = viewportW >> 1;
+            log(TAG, "viewportH: " + viewportH);
+            int centerY = ((viewportH - offsetTop) >> 1) + offsetTop;
+            mPaint.setColor(Color.parseColor("#ADADAD"));
+            mPaint.setStrokeWidth(2.5f);
+            mPaint.setStrokeCap(Paint.Cap.ROUND);
+            for (int i = 0; i < decibels.size(); i++) {
+                int x = zeroPoint + i * 5;
+                int db = decibels.get(i) < 5 ? 5 : decibels.get(i);
+                canvas.drawLine(x, centerY + db, x, centerY - db, mPaint);
+            }
         }
     }
 
@@ -235,7 +316,22 @@ public class TrackView extends FrameLayout {
         }
     }
 
-    public static int dipToPixels(Context context, int dip) {
+    public interface SlideGraduationListener {
+        void onGraduationChanged(int currentGraduation);
+    }
+
+    static int dipToPixels(Context context, int dip) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip, context.getResources().getDisplayMetrics());
+    }
+
+    @Nullable
+    static <T> T findViewById(@NonNull View view, @IdRes int id) {
+        return ((T) view.findViewById(id));
+    }
+
+    static void log(String tag, String message) {
+        if (DEBUG) {
+            Log.d(tag, message);
+        }
     }
 }
